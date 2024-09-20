abc707241e27c4f579de938d6ebb9ac94ddefdce
mack-aroni <etanm2013@gmail.com>
2024-09-19 23:36:20 -0400

    added middleware for specific api logging and potential auth

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server"
+import { authMiddleware } from "./middlewares/api/authMiddleware";
+import { logMiddleware } from "./middlewares/api/logMiddleware";
+
+export const config = {
+  matcher: "/api/:path*"
+}
+
+export default function middleware(request: Request) {
+  if (request.url.includes("/api/blogs")) {
+    const logResult = logMiddleware(request);
+    console.log(logResult.response);
+  }
+
+  const authResult = authMiddleware(request);
+  if (!authResult?.isValid && request.url.includes("/api/blogs")) {
+    return new NextResponse(
+      JSON.stringify({message: "Unauthorized"}),
+      {status: 401}
+    );
+  }
+  return NextResponse.next();
+}
